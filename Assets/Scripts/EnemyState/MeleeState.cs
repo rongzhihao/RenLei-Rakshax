b2920be202d19d477eded7dabe09409156140391
linx777 <kingxl777@gmail.com>
2018-09-12 15:54:15 -0700

    set up and finish map

---
+++ Assets/Scripts/EnemyState/MeleeState.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeState : IEnemyState
+{
+	private float shortAttackTimer;
+	private float shortAttackCoolDown = 0.3f;
+	private bool canAttack = true;
+	private Enemy enemy;
+    public void Enter(Enemy enemy)
+    {
+		this.enemy = enemy;
+        
+    }
+
+    public void Execute()
+    {
+       ShortAttack();
+	   if(enemy.InLongRange && ! enemy.InMeleeRange )
+	   {
+		   enemy.ChangeState(new RangedState());
+	   }
+    }
+
+    public void Exit()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider2D other)
+    {
+        
+    }
+
+	private void ShortAttack()
+	{
+		shortAttackTimer += Time.deltaTime;
+		if(shortAttackTimer >= shortAttackCoolDown)
+		{
+			shortAttackTimer = 0;
+			canAttack = true;
+		}
+		if(canAttack)
+		{
+			canAttack = false;
+			enemy.ShortAttack();
+		}
+	}
+}
---
+++ Assets/Scripts/Player/fireBall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class fireBall : MonoBehaviour {
+
+	// Use this for initialization
+	private Rigidbody2D myRigidbody;
+	private Vector2 direction;
+	[SerializeField]
+	private float speed = 20f;
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		myRigidbody.velocity = direction * speed; 
+	}
+
+	void OnBecameInvisible()
+	{
+		Destroy(gameObject);
+	}
+
+	public void initialize(Vector2 direction)
+	{
+		this.direction = direction;
+	}
+}
