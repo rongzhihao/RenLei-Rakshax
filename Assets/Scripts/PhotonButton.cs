25e987e86bf61cc30a9b31ddc493d8110afe8ba4
linx777 <kingxl777@gmail.com>
2018-09-19 17:23:25 -0700

    add script for connect server

---
+++ Assets/Scripts/PhotonButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonButton : MonoBehaviour {
+
+	public PhotonHandlers pHnadler;
+	public InputField createRoomInput, joinRoomInput, nameInput;
+
+	public void onClickCreateRoom(){
+		pHnadler.createNewRoom();
+	}
+
+	public void onClickJoinRoom(){
+		pHnadler.joinOrCreateRoom();
+	}
+}
---
+++ Assets/Scripts/PhotonHandlers.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PhotonHandlers : MonoBehaviour {
+
+	public PhotonButton photonB;
+
+	public GameObject mainPlayer;
+
+	private void Awake(){
+		
+		DontDestroyOnLoad(this.transform);
+		PhotonNetwork.sendRate = 30;
+		PhotonNetwork.sendRateOnSerialize = 20;
+		SceneManager.sceneLoaded += OnSceneFinishedLoading;
+	}
+
+	public void createNewRoom(){
+		PhotonNetwork.CreateRoom(photonB.createRoomInput.text, new RoomOptions(){MaxPlayers = 4}, null);
+	}
+
+	public void joinOrCreateRoom(){
+		RoomOptions roomOptions = new RoomOptions();
+		roomOptions.MaxPlayers = 4;
+		PhotonNetwork.JoinOrCreateRoom(photonB.joinRoomInput.text, roomOptions, TypedLobby.Default);
+	}
+	public void moveScene(){
+		PhotonNetwork.LoadLevel("Phototype");
+	}
+
+	private void OnJoinedRoom(){
+		moveScene();
+		Debug.Log("we are connected to the room");
+	}
+
+	private void OnSceneFinishedLoading(Scene scene, LoadSceneMode mode){
+		if(scene.name == "Phototype"){
+			spawnPlayer();
+		}
+	}
+
+	private void spawnPlayer(){
+		PhotonNetwork.Instantiate(mainPlayer.name, mainPlayer.transform.position, mainPlayer.transform.rotation, 0);
+	}
+}
