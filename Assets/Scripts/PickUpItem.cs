3e80c23ffa6b570a83815c42f95473908a4738bc
linx777 <kingxl777@gmail.com>
2018-10-08 17:41:25 -0700

    add pick up function

---
+++ Assets/Scripts/PickUpItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItem : MonoBehaviour {
+
+	private bool canBePickUp =false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		DestroyItem();
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		Debug.Log(other.tag);
+		if(other.tag == "Player"){
+
+			canBePickUp = true;
+		}
+	}
+
+	void DestroyItem () {
+		if(canBePickUp){
+			PhotonNetwork.Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/SpawBullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawBullet : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject redBullet;
+	[SerializeField]
+	private GameObject blueBullet;
+	// Use this for initialization
+	void Start () {
+		SpawBullets();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	private void SpawBullets (){
+        Vector3 bulletInitPlace = new Vector3(-1,1,0);
+        GameObject bullet = (GameObject)PhotonNetwork.Instantiate(redBullet.name, bulletInitPlace, Quaternion.Euler(new Vector3(0, 0, 180)), 0);
+	}
+}
