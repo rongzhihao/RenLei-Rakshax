edb17ddb2e58f9ae7ec9d039c1232a33ca37d007
Chen <42897513+chenyl4869@users.noreply.github.com>
2018-11-13 23:49:50 -0800

    when there is a bullet, no other bullet can be spawned any more

--- Assets/SpawBullet2.cs
+++ Assets/SpawBullet2.cs
@@ -36,7 +36,11 @@ public class SpawBullet2 : MonoBehaviour {
     {
         foreach (GameObject place in bulletPlaces)
         {
-            SpawSingleBullet(place.transform, bullet);          
+            if (!alreadyHasBullet(place.transform))
+            {
+                SpawSingleBullet(place.transform, bullet);
+            }
+                 
         }
     }
 
@@ -44,6 +48,21 @@ public class SpawBullet2 : MonoBehaviour {
     {
         Vector3 bulletInitPlace = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
         GameObject bullet1 = (GameObject)PhotonNetwork.Instantiate(BulletType.name, bulletInitPlace, Quaternion.Euler(new Vector3(0, 0, 180)), 0);
+    }
+
+    private bool alreadyHasBullet(Transform placeTransform)
+    {
+        Ray2D ray = new Ray2D(placeTransform.position, Vector2.up);
+        //Debug.DrawRay(ray.origin + new Vector2(0, 1), ray.direction, Color.blue,100000);
+        RaycastHit2D info = Physics2D.Raycast(ray.origin + new Vector2(0, 1), ray.direction,1f);
+
+        if (info.collider != null)
+        {
+            //Debug.Log("HasBullet:" + info.collider.name);
+            return true;
+        }
+        //Debug.Log("NoBullet");
+        return false;
     }
 
 }
